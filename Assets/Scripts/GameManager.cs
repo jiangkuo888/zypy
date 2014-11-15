df46c88b564d4543679c0e51fa554bfb93e3e502
jiangkuo888 <jiangkuo888@gmail.com>
2014-11-15 14:56:25 +0800

    jk fisrt commit/network done. create room/join room done/username done

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum GameStage {
+
+	waiting, distributeToAll, hostSelectQuestion, othersSelectFake, vote, resultRelease, showScore
+}
+
+public class GameManager : MonoBehaviour {
+	PhotonView myView;
+
+	public static GameManager SP;
+
+	public GameStage currentStage;
+
+	public List<Transform> pages;
+	// Use this for initialization
+	void Start () {
+		SP = this;
+		currentStage = GameStage.waiting;
+		LateUpdateStage (currentStage);
+		myView = GetComponent<PhotonView> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LateUpdateStage(GameStage Stage)
+	{
+
+		currentStage = Stage;
+
+		switch (Stage) 
+		{
+		case GameStage.waiting:
+			NGUITools.SetActive(pages[0].gameObject,true);
+			NGUITools.SetActive(pages[1].gameObject,false);
+			break;
+		case GameStage.distributeToAll:
+			NGUITools.SetActive(pages[0].gameObject,false);
+			NGUITools.SetActive(pages[1].gameObject,true);
+			break;
+		default:
+			break;
+		}
+	}
+
+
+	[RPC]
+	public void updateStage(int nextStage)
+	{
+
+		LateUpdateStage((GameStage)nextStage);
+
+	}
+
+	public void StartNewRound()
+	{
+
+		myView.RPC ("updateStage", PhotonTargets.All, (int)GameStage.distributeToAll);
+
+	}
+
+
+
+
+	public void OnApplicationQuit()
+	{
+		PhotonNetwork.LeaveRoom ();
+	}
+}
---
+++ Assets/Scripts/NetworkHandler.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class NetworkHandler : MonoBehaviour {
+	public string roomName = "JK's Room";
+	public UIInput PlayerInput;
+	public GameObject StartButton;
+	public UILabel WaitingString;
+	public UILabel PlayerNum;
+	
+	public bool AutoConnect = true;
+
+	public PhotonView myView;
+
+	public int PlayerNumber = 0;
+	
+	/// <summary>if we don't want to connect in Start(), we have to "remember" if we called ConnectUsingSettings()</summary>
+	private bool ConnectInUpdate = true;
+	
+	public virtual void Start()
+	{
+		PhotonNetwork.autoJoinLobby = false;    // we join randomly. always. no need to join a lobby to get the list of rooms.
+		NGUITools.SetActive (WaitingString.gameObject, false);
+
+
+		myView = GetComponent<PhotonView> ();
+	}
+	
+	public virtual void Update()
+	{
+		if (ConnectInUpdate && AutoConnect && !PhotonNetwork.connected)
+		{
+			//Debug.Log("Update() was called by Unity. Scene is loaded. Let's connect to the Photon Master Server. Calling: PhotonNetwork.ConnectUsingSettings();");
+			
+			ConnectInUpdate = false;
+			PhotonNetwork.ConnectUsingSettings("2."+Application.loadedLevel);
+		}
+
+		if (PlayerNumber != PhotonNetwork.playerList.Length) {
+			UpdatePlayerList();
+
+				}
+		
+	}
+	
+	// to react to events "connected" and (expected) error "failed to join random room", we implement some methods. PhotonNetworkingMessage lists all available methods!
+	
+	public virtual void OnConnectedToMaster()
+	{
+		if (PhotonNetwork.networkingPeer.AvailableRegions != null) Debug.LogWarning("List of available regions counts " + PhotonNetwork.networkingPeer.AvailableRegions.Count + ". First: " + PhotonNetwork.networkingPeer.AvailableRegions[0] + " \t Current Region: " + PhotonNetwork.networkingPeer.CloudRegion);
+		//Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
+		PhotonNetwork.JoinRandomRoom();
+	}
+	
+	public virtual void OnPhotonRandomJoinFailed()
+	{
+		//		Debug.Log("OnPhotonRandomJoinFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+		
+		Debug.Log ("Room created.");
+		PhotonNetwork.CreateRoom(null, new RoomOptions() { maxPlayers = 8 }, null);
+		
+		if(PhotonNetwork.isMasterClient)
+			NGUITools.SetActive(StartButton,true);
+	}
+	
+	// the following methods are implemented to give you some context. re-implement them as needed.
+	
+	public virtual void OnFailedToConnectToPhoton(DisconnectCause cause)
+	{
+		Debug.LogError("Cause: " + cause);
+	}
+	public void OnCreatedRoom()
+	{
+		UpdatePlayerList ();
+	}
+
+
+	public void OnJoinedRoom()
+	{
+		if(!PhotonNetwork.isMasterClient)
+			NGUITools.SetActive(StartButton,false);
+		
+		Debug.Log ("Room joined.");
+
+		UpdatePlayerList ();
+		//Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running. For reference, all callbacks are listed in enum: PhotonNetworkingMessage");
+	}
+	
+	public virtual void OnJoinedLobby()
+	{
+		Debug.Log("OnJoinedLobby(). Use a GUI to show existing rooms available in PhotonNetwork.GetRoomList().");
+	}
+
+	public void UpdatePlayerList()
+	{
+		if (PhotonNetwork.connected) {
+			PlayerNum.text = "Players: ";
+			
+			for (int i=0; i<PhotonNetwork.playerList.Length; i++) {
+				PlayerNum.text += "\n" + PhotonNetwork.playerList [i].name;
+			}
+		}
+
+		PlayerNumber = PhotonNetwork.playerList.Length;
+	}
+	
+	// submit local player name
+	public void SetPlayerName()
+	{
+		if (PhotonNetwork.connected) {
+			PhotonNetwork.playerName = PlayerInput.value;
+			print (PhotonNetwork.playerName);
+		}
+		
+		NGUITools.SetActive (PlayerInput.gameObject, false);
+		NGUITools.SetActive (WaitingString.gameObject, true);
+		
+	}
+}
